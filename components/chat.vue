5fa1c54a712b07798518b9e9a553b940279a6a1a
Эдуард Егоров <egedw@mail.ru>
2019-06-19 23:26:27 +0300

    getTypeMessage fixed, avater added, antoher color for bot added two side message added

--- components/chat.vue
+++ components/chat.vue
@@ -30,14 +30,9 @@
       },
       onSendMessage(event) {
         const date = new Date();
-        const type = this.getTypeMessage(event);
-        if (type=='video'){
-          message = message.replace("watch?v=", "embed/")
-          console.log(message);
-        }
         this.$bus.send('/common', {
           id: date.getTime(),
-          type: type,
+          type: 'chat_message',
           body: {
             sender: 'user',
             date: date.toUTCString(),
@@ -45,53 +40,40 @@
           }
         })
         const answer = [
-          'Привет, как у тебя дела?',
-          'https://www.google.ru/',
-          'https://www.ebay.com/',
-          'https://ru.aliexpress.com',
-          'https://trinixy.ru/pics3/20080124/podb/6/krasota_01.jpg',
-          'https://pp.userapi.com/c639419/v639419834/49f2b/_IhYYjUstAY.jpg?ava=1',
-          'Я проверка не бот ',
-          'lorem ipsum set emmet ',
+          // 'Привет, как у тебя дела?',
+          // 'https://www.google.ru/',
+          // 'https://www.ebay.com/',
+          // 'https://ru.aliexpress.com',
+          // 'https://trinixy.ru/pics3/20080124/podb/6/krasota_01.jpg',
+          // 'https://pp.userapi.com/c639419/v639419834/49f2b/_IhYYjUstAY.jpg?ava=1',
+          // 'Я проверка не бот ',
+          // 'lorem ipsum set emmet ',
           'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
           'https://www.youtube.com/watch?v=ZhIsAZO5gl0',
           'https://www.youtube.com/watch?v=n4RjJKxsamQ',
           'https://www.youtube.com/watch?v=hTWKbfoikeg',
-          'Text 2',
-          'Text 3',
-          'Text 4',
+          // 'Text 2',
+          // 'Text 3',
+          // 'Text 4',
         ]
         setTimeout(() => {
           const dateBot = new Date();
-          const messageBot = answer[Math.floor(Math.random()*answer.length)]
-          const typeBot = this.getTypeMessage(messageBot);
-        if (typeBot=='video'){
-          messageBot = messageBot.replace("watch?v=", "embed/");
-          console.log(messageBot);
-        }
+          let messageBot = answer[Math.floor(Math.random()*answer.length)]
           this.$bus.send('/common', {
-            id: dateBot.getTime(),
-            type: typeBot,
+            id: dateBot.getTime()+'_bot',
+            type: 'chat_message',
             body: {
-              sender: 'bot',
+              sender: {
+                name: 'bot',
+                avatarSrc: 'https://trinixy.ru/pics3/20080124/podb/6/krasota_01.jpg'
+                },
               date: dateBot.toUTCString(),
               text: messageBot
             }
           })
           }, 300) 
       },
-      getTypeMessage(message){
-        if ( /^[^\s]+(?:\.(com|ru|net|org))[^\s]*/.test(message)){
-          if ( /^[^\s]+(?:\.(com|ru|net|org))[^\s]*\.(png|jpg|jpeg)/.test(message)){
-            return 'image'
-          }
-          if ( /^https\:\/\/www\.youtube\.com\/watch\?v\=[a-zA-Z0-9_]+/.test(message)){
-            return 'video'
-          } 
-          return 'link'
-        }
-        return 'text'
-      }
+
     }
   }
 </script>
--- components/messages.vue
+++ components/messages.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="chat__messages">
      <div class="chat__messages__message" v-for="message of chatMessages" v-bind:key="message.id" v-bind:class="{'bot':message.body.sender=='bot'}">
-       <span>[{{message.body.date}}] from {{message.body.sender}}</span>
+       <span>[{{message.body.date}}] from <img :src="message.body.sender.avatarSrc"></span>
        <br>
-       <img v-if="message.type=='image' " :src="message.body.text" alt="image">
-       <a v-if="message.type=='link'" :href="message.body.text">{{message.body.text}}</a>
-       <iframe v-if="message.type=='video'"  :src="message.body.text" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-       <span v-if="message.type=='text'">{{message.body.text }}</span>
+       <img v-if="message.body.type=='image' " :src="message.body.text" alt="image">
+       <a v-if="message.body.type=='link'" :href="message.body.text">{{message.body.text}}</a>
+       <iframe v-if="message.body.type=='video'"  :src="message.body.text" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+       <span v-if="message.body.type=='text'">{{message.body.text }}</span>
      </div>
     </div>
 </template>
@@ -22,6 +22,32 @@
       chatMessages() {
         // return this.messages.filter(m => m.type === 'chat_message')
          return this.messages
+      }
+    },
+    methods:{
+      getTypeMessage(message){
+        if ( /^[^\s]+(?:\.(com|ru|net|org))[^\s]*/.test(message)){
+          if ( /^[^\s]+(?:\.(com|ru|net|org))[^\s]*\.(png|jpg|jpeg)/.test(message)){
+            return 'image'
+          }
+          if ( /^https\:\/\/www\.youtube\.com\/watch\?v\=[a-zA-Z0-9_]+/.test(message)){
+            return 'video'
+          } 
+          return 'link'
+        }
+        return 'text'
+      }
+    },
+    watch:{
+      messages(){
+        if (this.messages.length>0){
+          let lastMessage = this.messages[this.messages.length-1]
+          lastMessage.body.type = this.getTypeMessage(lastMessage.body.text)
+          if (lastMessage.body.type=='video'){
+           lastMessage.body.text = lastMessage.body.text.replace("watch?v=", "embed/")
+          }
+          this.messages[this.messages.length-1] = lastMessage
+        }
       }
     }
   }
